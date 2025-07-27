2281f4878e5a7180f1e3e335b1f1fb24142357cd
might.ncube <might.ncube@teampicnic.com>
2025-07-27 23:09:23 +0200

    First commit

---
+++ Testofgit.py
@@ -0,0 +1,15 @@
+print  ("Hello World,, testing Git Concepts")
+
+one = 23
+two = 32
+
+
+answer = one - two
+
+def test_function():
+    print(answer)
+
+test_function()
+
+git config --global user.email "might.ncube@teampicnic.com"
+git config --global user.name "might.ncube"
